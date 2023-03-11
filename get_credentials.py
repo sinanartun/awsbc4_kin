fa66b9ce288f79ae2475edc72dafe613773af2af
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-03-11 14:11:09 +0100

    update

--- get_credentials.py
+++ get_credentials.py
@@ -1,6 +1,6 @@
 import requests
 
-
+import time
 def get():
     # retrieve the IAM role name
     url = "http://169.254.169.254/latest/meta-data/iam/security-credentials/"
@@ -10,6 +10,8 @@ def get():
     token_url = "http://169.254.169.254/latest/api/token"
     headers = {"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
     token = requests.put(token_url, headers=headers).text.strip()
+    # print('000')
+    # print(token)
 
     # retrieve security credentials
     security_credentials_url = f"http://169.254.169.254/latest/meta-data/iam/security-credentials/{role_name}"
@@ -18,7 +20,8 @@ def get():
 
     # parse the security credentials
     security_credentials = response.json()
-    print(security_credentials)
+    # print(security_credentials)
+
     return security_credentials
 
     # print the security credentials
