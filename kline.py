ca6215af1c233e83fbcc25589397a3cc131a8d3f
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-03-22 19:06:03 +0100

    update

--- kline.py
+++ kline.py
@@ -13,13 +13,13 @@ api_key = 'bXUPD49c3jXqEHKkbHvn7hrBNXz2zX0hQHyfVuR6Y8XNFn37teQRIIRjVmy303KC'
 api_secret = 'jsIKSMC6znLU8u7gfh0FQ6Ho0Xa0oFzJ8qca5N5ic2GBgrLnWRNuyy1Ubski6Z2A'
 last_time = 0
 credentials = get_credentials.get()
-kinesis_client = boto3.client(
-    'kinesis',
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['Token'],
-    region_name='eu-north-1'
-)
+# kinesis_client = boto3.client(
+#     'kinesis',
+#     aws_access_key_id=credentials['AccessKeyId'],
+#     aws_secret_access_key=credentials['SecretAccessKey'],
+#     aws_session_token=credentials['Token'],
+#     region_name='eu-north-1'
+# )
 
 
 def main():
@@ -38,15 +38,18 @@ def main():
             data += str(msg['k']['l']) + ","
             data += str(msg['k']['c']) + ","
             data += str(msg['k']['v']) + ","
-            data += str(msg['k']['T']) + "," + ta.main()
-            try:
-                response = kinesis_client.put_record(StreamName='kline', Data=data, PartitionKey=str(msg['k']['t']))
+            data += str(msg['k']['T'])
 
-            except ClientError:
-                print("Couldn't put record in stream 'binance'")
-                raise
-            else:
-                print(response)
+            print(data)
+            # data += str(msg['k']['T']) + "," + ta.main()
+            # try:
+            #     response = kinesis_client.put_record(StreamName='kline', Data=data, PartitionKey=str(msg['k']['t']))
+            #
+            # except ClientError:
+            #     print("Couldn't put record in stream 'binance'")
+            #     raise
+            # else:
+            #     print(response)
 
     twm.start_kline_socket(callback=handle_socket_message, symbol=symbol, interval='1m')
 
