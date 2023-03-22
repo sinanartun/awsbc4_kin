fa55b000a81998a601aca9bbff670b844a3463e6
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2023-03-22 19:21:48 +0100

    update

--- kline.py
+++ kline.py
@@ -13,6 +13,7 @@ api_key = 'bXUPD49c3jXqEHKkbHvn7hrBNXz2zX0hQHyfVuR6Y8XNFn37teQRIIRjVmy303KC'
 api_secret = 'jsIKSMC6znLU8u7gfh0FQ6Ho0Xa0oFzJ8qca5N5ic2GBgrLnWRNuyy1Ubski6Z2A'
 last_time = 0
 credentials = get_credentials.get()
+last_data = None
 # kinesis_client = boto3.client(
 #     'kinesis',
 #     aws_access_key_id=credentials['AccessKeyId'],
@@ -22,7 +23,9 @@ credentials = get_credentials.get()
 # )
 
 
+
 def main():
+
     symbol = 'BTCUSDT'
 
     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
@@ -30,16 +33,17 @@ def main():
     twm.start()
 
     def handle_socket_message(msg):
+        kline.last_data = msg['k']
         if msg['k']['t'] != kline.last_time:
             print(msg['k'])
-            kline.last_time = msg['k']['t']
-            data = str(msg['k']['t']) + ","
-            data += str(msg['k']['o']) + ","
-            data += str(msg['k']['h']) + ","
-            data += str(msg['k']['l']) + ","
-            data += str(msg['k']['c']) + ","
-            data += str(msg['k']['v']) + ","
-            data += str(msg['k']['T'])
+            kline.last_time = kline.last_data['t']
+            data = str(kline.last_data['t']) + ","
+            data += str(kline.last_data['o']) + ","
+            data += str(kline.last_data['h']) + ","
+            data += str(kline.last_data['l']) + ","
+            data += str(kline.last_data['c']) + ","
+            data += str(kline.last_data['v']) + ","
+            data += str(kline.last_data['T'])
 
             print(data)
             # data += str(msg['k']['T']) + "," + ta.main()
